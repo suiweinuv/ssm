7047b70486e59a2f83724bc1f064b61fdf2abc0d
suiwei <136693118@qq.com>
2020-04-08 23:04:16 +0800

    utils

---
+++ src/main/java/com.cm/util/StringUtils.java
@@ -0,0 +1,13 @@
+package com.cm.util;
+
+public class StringUtils {
+    //截取最后一位
+    public static  String substrLast(String s){
+        String result ="";
+        //一定要写result = ，否则s截取完了之后，s本身是不变化的
+        result = s.substring(0,s.length()-1);
+
+        return  result;
+    }
+
+}
